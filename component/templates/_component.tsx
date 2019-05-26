3d181bdb3aa9f5ed01df90d1ca1620e13f744bfb
Thomas England <personal@england.codes>
2019-05-26 14:33:02 +0100

    version/fix(shadowDom): incremented version, turned shadowDom on by default for components, fixed annoying whitespace on scss

--- component/templates/_component.tsx
+++ component/templates/_component.tsx
@@ -3,7 +3,7 @@ import { Component, h } from '@stencil/core';
 @Component({
   tag: '<%= paramCaseComponentName %>',
   styleUrl: '<%=paramCaseComponentName%>.<% if(stylingSupport.includes("Sass")) { %>scss<% } %><% if(!stylingSupport.includes("Sass")) { %>css<% } %>',
-  shadow: false
+  shadow: true
 })
 export class <%=componentName%> {
   render() {
